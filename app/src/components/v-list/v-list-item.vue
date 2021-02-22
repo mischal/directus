1ba89c6cc000f13b6d464c11350c41a2d72234be
rijkvanzanten <rijkvanzanten@me.com>
2021-02-22 16:04:36 -0500

    Render basic tree in tree-view

--- app/src/components/v-list/v-list-item.vue
+++ app/src/components/v-list/v-list-item.vue
@@ -105,6 +105,7 @@ body {
 	--v-list-item-color: var(--v-list-color, var(--foreground-normal));
 	--v-list-item-color-hover: var(--v-list-color-hover, var(--foreground-normal));
 	--v-list-item-color-active: var(--v-list-color-active, var(--foreground-normal));
+	--v-list-item-background-color: transparent;
 	--v-list-item-background-color-hover: var(--v-list-background-color-hover, var(--background-normal-alt));
 	--v-list-item-background-color-active: var(--v-list-background-color-active, var(--background-normal-alt));
 }
@@ -131,6 +132,7 @@ body {
 	color: var(--v-list-item-color);
 	text-decoration: none;
 	border-radius: var(--v-list-item-border-radius);
+	background-color: var(--v-list-item-background-color);
 
 	&.dashed {
 		&::after {
